96d06d5a158dcf9e40ff254b1f12cf914fd2cdd2
Abdul Rafay Modi <subjectalpha32@gmail.com>
2021-04-16 15:35:14 +0500

    User Data is being added :person:

--- Entity Framework Code First Approach/Controllers/StudentsController.cs
+++ Entity Framework Code First Approach/Controllers/StudentsController.cs
@@ -15,6 +15,30 @@ namespace Entity_Framework_Code_First_Approach.Controllers
         {
             var data = db.Students.ToList();
             return View(data);
+        }        
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if(ModelState.IsValid == true) 
+            {
+                db.Students.Add(s);
+                int a = db.SaveChanges();
+                
+                if(a > 0)
+                {
+                    TempData["Message"] = "<p class=\"alert alert-success col-12\">The data was inserted</p>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Message = "<p class=\"alert alert-danger col-12\">The data was deleted</p>";
+                }   
+            }
+            return View();
         }
     }
 }
