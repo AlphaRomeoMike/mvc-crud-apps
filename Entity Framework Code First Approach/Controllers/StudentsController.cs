4f3209fe488e9a3dc5c610fab49a7bb895e3e40c
ARM <subjectalpha32@gmail.com>
2021-04-16 17:33:14 +0500

    CUD Ops complete :boom:

--- Entity Framework Code First Approach/Controllers/StudentsController.cs
+++ Entity Framework Code First Approach/Controllers/StudentsController.cs
@@ -1,9 +1,7 @@
 ﻿using Entity_Framework_Code_First_Approach.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Entity_Framework_Code_First_Approach.Controllers
 {
@@ -39,6 +37,50 @@ namespace Entity_Framework_Code_First_Approach.Controllers
                 }   
             }
             return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            var row = db.Students.Where(model => model.Id == id).FirstOrDefault();
+            return View(row);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {
+            if(ModelState.IsValid == true)
+            {
+                db.Entry(s).State = EntityState.Modified;
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["Update"] = "<p class=\"alert alert-success col-12\">The data was updated</p>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.UpdateMessage = "<p class=\"alert alert-danger col-12\">The data was not updated</p>";
+                }
+            }
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            var row = db.Students.Where(model => model.Id == id).FirstOrDefault();
+            return View(row);
+        }
+        [HttpPost]
+        public ActionResult Delete(Student s)
+        {
+            db.Entry(s).State = EntityState.Deleted;
+            int a = db.SaveChanges();
+            if(a > 0)
+            {
+                TempData["Delete"] = "<p class=\"alert alert-danger\">The data has been deleted</p>";
+            }
+            else
+            {
+                TempData["Delete"] = "<p class\"alert alert-danger\">The data was not deleted</p>";
+            }
+            return RedirectToAction("Index"); 
         }
     }
 }
